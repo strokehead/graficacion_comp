faa7f72beab30ec34a397410b161775043bbf4e1
Odalis <odalisgarcia7736@gmail.com>
2022-09-28 12:18:25 -0400

    Added Entry

--- WinTkinter.py
+++ WinTkinter.py
@@ -1,5 +1,6 @@
 from ctypes import sizeof
 import tkinter
+from typing_extensions import IntVar
 
 
 win = tkinter.Tk()
@@ -19,16 +20,39 @@ def botonBr():
 
 #win.grid_rowconfigure(0, weight=3)
 win.grid_columnconfigure(0, weight=1)
+win.grid_columnconfigure(1, weight=2)
+win.grid_columnconfigure(2, weight=2)
 
 titleW = tkinter.Label(win, text= "ALGORITMOS DE LÍNEA", font= "Arial 20")
-#titleW.pack()
-buttonE = tkinter.Button(win, text = "Ecuación cartesiana", font= "Helvetica 14", height= "2", width= "20")
-#buttonE.pack()
-buttonB = tkinter.Button(win, text = "De Bresenham", font= "Helvetica 14", height= "2", width= "20")
-#buttonB.pack()
 
-titleW.grid  (row = 0, column= 0, pady = (30,0))
-buttonE.grid (row = 1, column= 0, pady = (70,20))
-buttonB.grid (row = 2, column= 0)
+lblP1 = tkinter.Label(win, text= "Punto 1", font= "Helvetica 10")
+lblP2 = tkinter.Label(win, text= "Punto 2", font= "Helvetica 10")
+
+valueX1 = tkinter.IntVar()
+valueY1 = tkinter.IntVar()
+valueX2 = tkinter.IntVar()
+valueY2 = tkinter.IntVar()
+
+entryX1 = tkinter.Entry(win, textvariable=valueX1)
+entryY1 = tkinter.Entry(win, textvariable=valueY1)
+entryX2 = tkinter.Entry(win, textvariable=valueX2)
+entryY2 = tkinter.Entry(win, textvariable=valueY2)
+
+buttonE = tkinter.Button(win, text = "Ecuación cartesiana", font= "Helvetica 14", height= "2", width= "20")
+buttonB = tkinter.Button(win, text = "De Bresenham", font= "Helvetica 14", height= "2", width= "20")
+
+#titleW.grid  (row = 0, column= 0, columnspan=2, pady = (30,0))
+titleW.grid  (row = 0, column= 0, columnspan=3, pady = (30,10))#, sticky=tkinter.EW)
+
+lblP1.grid  (row= 1, column= 0)
+entryX1.grid(row= 1, column= 1, sticky='E')
+entryY1.grid(row= 1, column= 2, sticky='W')
+lblP2.grid  (row= 2, column= 0)
+entryX2.grid(row= 2, column= 1, sticky='E')
+entryY2.grid(row= 2, column= 2, sticky='W')
+
+buttonE.grid (row = 3, column= 0, columnspan=3, pady = (70,20))
+buttonB.grid (row = 4, column= 0, columnspan=3)
+#frame.place(relx)
 
 win.mainloop()
