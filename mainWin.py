77dc5f3fb1086aa69a1c33dc690da13fff3f343d
Alex Alvarez <alvarez.rojas.alexander.james5@gmail.com>
2022-10-07 22:48:06 -0400

    union (falta probar)

--- mainWin.py
+++ mainWin.py
@@ -3,14 +3,51 @@ from struct import pack
 import tkinter as tk
 from typing_extensions import IntVar
 from PIL import Image, ImageTk
+from sympy import capture
 
-def iconR(nameIcon):
+import square_options as isp
+import triangle_options as itp
+import circle_options as crp
+
+import drawArea as da
+
+def iconxR(nameIcon):
     dir = "icons/{}.png".format(nameIcon)
     icon = Image.open(dir)
     icon = icon.resize((20,20))
     icon = ImageTk.PhotoImage(icon)
     return icon
 
+
+def options_square():
+    capt = []
+    aux = isp.popup_sqo(capt,winMain)
+    aux.top_level.wait_window()
+    if len(capt)!=0:
+        psqr = []
+        p1 = (capt[0], capt[1])
+        psqr.append(p1)
+        psqr.append((p1[0]+capt[2],p1[1]))
+        psqr.append((p1[0]+capt[2], p1[1]+capt[2]))
+        psqr.append((p1[0],p1[1]+capt[2]))
+        points = []
+        dd.draw(points)
+
+def options_triangle():
+    capt = []
+    aux = itp.popup_tro(capt, winMain)
+    aux.top_level.wait_window
+    if len(capt)!=0:
+        points = []
+        dd.draw(points)
+
+def options_circle():
+    capt = []
+    aux = crp.popup_cro(capt, winMain)
+    aux.top_level.wait_window
+    if len(capt)!=0:
+        point = list(())
+        dd.draw(point)
 winMain = tk.Tk()
 
 # winMain.state('zoomed')
@@ -19,15 +56,15 @@ winMain.geometry("1000x800")
 winMain.minsize(500,500)
 
 #winMain.grid_rowconfigure(0, weight=3)
-# winMain.grid_columnconfigure(0, weight=1)
-# winMain.grid_columnconfigure(1, weight=2)
-# winMain.grid_columnconfigure(2, weight=2)
+#winMain.grid_columnconfigure(0, weight=1)
+#winMain.grid_columnconfigure(1, weight=2)
+#winMain.grid_columnconfigure(2, weight=2)
 
 titleW = tk.Label(winMain, text= "ALGORITMOS DE LÍNEA", font= "Arial 20")
 
-iconSquare   = iconR("square")
-iconCircle   = iconR("circle")
-iconTriangle = iconR("triangle")
+iconSquare   = iconxR("square")
+iconCircle   = iconxR("circle")
+iconTriangle = iconxR("triangle")
 
 menus = tk.Menu()
 menuArchive = tk.Menu(menus, tearoff = False)
@@ -50,18 +87,33 @@ menus.add_cascade(menu = menuOptions, label = "Opciones")
 
 winMain.config(menu = menus)
 
-frameMenu    = tk.Frame(winMain, height = 20, relief = "raised", borderwidth = 1)
-frameButtons = tk.Frame(winMain, width  = 30, relief = "raised", borderwidth = 1)
+#frameMenu    = tk.Frame(winMain, height = 20, relief = "raised", borderwidth = 1)
+#frameButtons = tk.Frame(winMain, width  = 30, relief = "raised", borderwidth = 1)
 
-buttonS = tk.Button(frameButtons, image = iconSquare)
-buttonC = tk.Button(frameButtons, image = iconCircle)
-buttonT = tk.Button(frameButtons, image = iconTriangle)
+frameMenu       = tk.Frame(winMain)
+frameButtons    = tk.Frame(frameMenu)
 
-frameMenu.grid(row = 0, column = 0)
-frameButtons.grid(row = 1, column = 0)
+buttonS = tk.Button(frameButtons, image = iconSquare, command=options_square)
+buttonC = tk.Button(frameButtons, image = iconCircle, command=options_circle)
+buttonT = tk.Button(frameButtons, image = iconTriangle, command=options_triangle)
 
-buttonS.grid(row = 0, column = 0, padx = (0,3), pady = (3,3))
-buttonC.grid(row = 1, column = 0, padx = (0,3), pady = (3,3))
-buttonT.grid(row = 2, column = 0, padx = (0,3), pady = (3,3))
+buttonS.pack(side=tk.LEFT)
+buttonC.pack(side=tk.LEFT)
+buttonT.pack(side=tk.LEFT)
+frameButtons.pack(side=tk.LEFT)
+frameMenu.pack(side=tk.TOP)
+#frameMenu.grid(row = 0, column = 0)
+#frameButtons.grid(row = 1, column = 0)
+
+#buttonS.grid(row = 0, column = 0, padx = (0,3), pady = (3,3))
+#buttonC.grid(row = 1, column = 0, padx = (0,3), pady = (3,3))
+#buttonT.grid(row = 2, column = 0, padx = (0,3), pady = (3,3))
+
+canv = tk.Canvas(master=winMain,height=400, width=500)
+# aqui realizamos los pasos para poder pintar un objeto
+
+
+x = [[5,5],[5,6],[7,6],[8,9],[2,5],[2,3],[2,4],[2,3]]
+dd.draw(x, 'red')
 
 winMain.mainloop()
---
+++ triangle_options.py
@@ -0,0 +1,91 @@
+from tkinter import *
+from tkinter import messagebox
+
+class popup_tro(Frame):
+    def __init__(self, out_val:list, master: Tk, *args, **kargs):
+        self.out = out_val
+        super().__init__(master=master, *args, **kargs)
+        self.top_level = Toplevel(master)
+        #incluir botones para entradas, etc.
+        
+        self.puntos_input()
+
+        self.grosor_color_frame()
+        
+        self.segmentado_input()
+        #fin modificaciones
+        Button(master=self.top_level, text='Cancelar', command=self.destroy).pack(side=RIGHT)
+        Button(master = self.top_level, text='Aceptar', command=self.save_destroy).pack(side=RIGHT)
+        self.top_level.protocol('MW_DELETE_WINDOW', self.destroy)
+    
+    def puntos_input(self):
+        self.entt_impo = Frame(master=self.top_level)
+        self.a = Label(master=self.entt_impo, text='Punto A:')
+        self.a.pack(side=LEFT)
+        self.a_in_x = Entry(master=self.entt_impo)
+        self.a_in_x.pack(side=LEFT)
+        self.a_in_y = Entry(master=self.entt_impo)
+        self.a_in_y.pack(side=LEFT)
+        self.b = Label(master=self.entt_impo, text='Punto B:')
+        self.b.pack(side=LEFT)
+        self.b_in_x = Entry(master=self.entt_impo)
+        self.b_in_x.pack(side=LEFT)
+        self.b_in_y = Entry(master=self.entt_impo)
+        self.b_in_y.pack(side=LEFT)
+        self.c = Label(master=self.entt_impo, text='Punto C:')
+        self.c.pack(side=LEFT)
+        self.c_in_x = Entry(master=self.entt_impo)
+        self.c_in_x.pack(side=LEFT)
+        self.c_in_y = Entry(master=self.entt_impo)
+        self.c_in_y.pack(side=LEFT)
+        self.entt_impo.pack(side=TOP)
+
+    def grosor_color_frame(self):
+        self.gro_col = Frame(master=self.top_level)
+        Label(master=self.gro_col, text='grosor:').pack(side=LEFT)
+        var = StringVar(self.gro_col)
+        self.grosor = OptionMenu(self.gro_col,var, '1', '2', '3',command=self.redef)
+        var.set('1')
+        self.grosor.pack(side=LEFT)
+        Label(master=self.gro_col, text='color: ').pack(side=LEFT)
+        self.color = Entry(master=self.gro_col)
+        self.color.pack(side=LEFT)
+        Button(master=self.gro_col, text='cod_col', command=self.destroy).pack(side=RIGHT)
+        self.gro_col.pack(side=TOP)
+
+    def segmentado_input(self):
+        self.seg = Frame(master=self.top_level)
+        Label(master=self.seg, text='Segmentado').pack(side=LEFT)
+
+        self.segmentado = Checkbutton(master=self.seg).pack(side=LEFT)
+        
+        self.seg.pack(side=TOP)
+
+    def redef(self, selection):
+        self.grosor_out = selection
+
+    def destroy(self):
+        self.top_level.destroy()
+    
+    def save_destroy(self):
+        try:
+            self.out.append(int(self.a_in_x.get()))
+            self.out.append(int(self.a_in_y.get()))
+            self.out.append(int(self.b_in_x.get()))
+            self.out.append(int(self.b_in_y.get()))
+            self.out.append(int(self.c_in_x.get()))
+            self.out.append(int(self.c_in_y.get()))
+            app = []
+            app.append(int(self.grosor_out))
+            app.append(self.color)
+            app.append(self.segmentado.get())
+            self.out.append(app)
+            print(self.out)
+            self.destroy()
+        except ValueError:
+            self.mostrarError('Error en las entradas, solo admite numeros')
+        except Exception:
+            self.mostrarError('Error debe llenar las areas requeridas')
+
+    def mostrarError(self, x:str):
+        messagebox.showerror(title='Error de entrada', message=x)
